09ac685249e3f190086aef4fd6a0b217356d4252
여명구 <88411212+brighteen@users.noreply.github.com>
2025-01-16 01:40:06 +0000

    250116 10:40

---
+++ 10_여명구/03_OutputParser/03_Structured.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+from langchain.output_parsers import ResponseSchema, StructuredOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
+LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
+LANGCHAIN_ENDPOINT = os.getenv("LANGCHAIN_ENDPOINT")
+LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")
+
+# 사용자의 질문에 대한 답변
+response_schemas = [
+    ResponseSchema(name="answer", description="사용자의 질문에 대한 답변"),
+    ResponseSchema(
+        name="source",
+        description="사용자의 질문에 답하기 위해 사용된 `출처`, `웹사이트주소` 이여야 합니다.",
+    ),
+]
+# 응답 스키마를 기반으로 한 구조화된 출력 파서 초기화
+output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+
+# 출력 형식 지시사항을 파싱합니다.
+format_instructions = output_parser.get_format_instructions()
+prompt = PromptTemplate(
+    # 사용자의 질문에 최대한 답변하도록 템플릿을 설정합니다.
+    template="answer the users question as best as possible.\n{format_instructions}\n{question}",
+    # 입력 변수로 'question'을 사용합니다.
+    input_variables=["question"],
+    # 부분 변수로 'format_instructions'을 사용합니다.
+    partial_variables={"format_instructions": format_instructions},
+)
+
+model = ChatOpenAI(model="gpt-4o", temperature=0)
+chain = prompt | model | output_parser
+
+response = chain.invoke({"question": "오늘 며칠이야?"})
+print(response)

ec53a82c9c21615d1efbe91868744e54ecb126bd
Yu Sangmin <dbtkd1102@gmail.com>
2025-01-17 08:22:02 +0000

    embeddings_isascii삭제

--- 04-유상민/250117과제_OpenAIEmbeddings/main.py
+++ 04-유상민/250117과제_OpenAIEmbeddings/main.py
@@ -25,13 +25,7 @@ sentences = [sentence1, sentence2, sentence3, sentence4, sentence5]
 # target='en'은 번역할 대상 언어를 영어로 설정
 translator = GoogleTranslator(source='ko', target='en')
 
-# 한글 문장을 영어로 번역하거나, 영어 문장은 그대로 유지
-translated_sentences = [
-    translator.translate(sentence)  # 한글 문장은 영어로 번역
-    if not sentence.isascii()      # 문장이 ASCII 문자가 아닐 때 (한글일 때)
-    else sentence                  # 문장이 ASCII 문자일 경우 (영어일 경우) 그대로 사용
-    for sentence in sentences      # sentences 리스트의 모든 문장을 순회하며 처리
-]
+translated_sentences = [translator.translate(sentence) for sentence in sentences]
 
 # 번역된 문장으로 임베딩 생성
 embedded_sentences = scaled_embeddings.embed_documents(translated_sentences)

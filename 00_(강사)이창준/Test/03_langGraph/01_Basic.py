aa35771b450621ad43a59339baa5ec894f6cc6bd
LEE CHANG-JUN <125140507+cjLee-cmd@users.noreply.github.com>
2025-01-26 22:32:14 +0900

    새로운 LangSmith 로깅 기능 추가 및 API 키 환경변수 설정

---
+++ 00_(강사)이창준/Test/03_langGraph/01_Basic.py
@@ -0,0 +1,60 @@
+# Setup------------------------------------------
+# 0.0 API 키를 환경변수로 관리하기 위한 설정 파일
+import os
+from dotenv import load_dotenv
+
+# API 키 정보 로드
+load_dotenv()
+
+# 0.1 LangSmith 로깅 설정
+from newstool import logging
+
+# 프로젝트 이름을 입력합니다.
+logging.langsmith("ls_JJU_langGraph_01")
+
+# print(os.environ['OPENAI_API_KEY'])
+# ------------------------------------------------
+
+from typing import Annotated, TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+
+## State 정의
+class State(TypedDict):
+    # 메시지 정의(list type 이며 add_messages 함수를 사용하여 메시지를 추가)
+    messages: Annotated[list, add_messages]  
+
+from langchain_openai import ChatOpenAI
+
+## Node 정의
+llm = ChatOpenAI(model = "gpt-4o", temperature = 0)
+
+def chatbot(state:State):
+    return {"messages":[llm.invoke  (state["messages"])]}
+
+## graph 정의
+graph_builder = StateGraph(State)
+
+## 노드 추가
+graph_builder.add_node("chatbot", chatbot)
+
+## 엣지 추가
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+## 그래프 컴파일
+graph = graph_builder.compile()
+
+# ## 그래프 시각화
+# from graphs.graphs import visualize_graph_mermaid
+
+# visualize_graph_mermaid(graph_builder)
+
+## 그래프 실행
+query = "서울의 맛집 추천?"
+
+# 그래프 이벤트 스트리밍
+for event in graph.stream({"messages": [("user", query)]}):
+    # 이벤트 값 출력
+    for value in event.values():
+        print("Assistant:", value["messages"][-1].content)
---
+++ 00_(강사)이창준/Test/03_langGraph/graphs/graphs.py
@@ -0,0 +1,95 @@
+import os
+import random
+from IPython.display import Image, display
+from langgraph.graph.state import CompiledStateGraph
+from dataclasses import dataclass
+from langgraph.graph import StateGraph
+
+
+@dataclass
+class NodeStyles:
+    default: str = (
+        "fill:#45C4B0, fill-opacity:0.3, color:#23260F, stroke:#45C4B0, stroke-width:1px, font-weight:bold, line-height:1.2"  # 기본 색상
+    )
+    first: str = (
+        "fill:#45C4B0, fill-opacity:0.1, color:#23260F, stroke:#45C4B0, stroke-width:1px, font-weight:normal, font-style:italic, stroke-dasharray:2,2"  # 점선 테두리
+    )
+    last: str = (
+        "fill:#45C4B0, fill-opacity:1, color:#000000, stroke:#45C4B0, stroke-width:1px, font-weight:normal, font-style:italic, stroke-dasharray:2,2"  # 점선 테두리
+    )
+
+
+def visualize_graph(graph, xray=False):
+    """
+    CompiledStateGraph 객체를 시각화하여 표시합니다.
+
+    이 함수는 주어진 그래프 객체가 CompiledStateGraph 인스턴스인 경우
+    해당 그래프를 Mermaid 형식의 PNG 이미지로 변환하여 표시합니다.
+
+    Args:
+        graph: 시각화할 그래프 객체. CompiledStateGraph 인스턴스여야 합니다.
+
+    Returns:
+        None
+
+    Raises:
+        Exception: 그래프 시각화 과정에서 오류가 발생한 경우 예외를 출력합니다.
+    """
+    try:
+        # 그래프 시각화
+        if isinstance(graph, CompiledStateGraph):
+            display(
+                Image(
+                    graph.get_graph(xray=xray).draw_mermaid_png(
+                        background_color="white",
+                        node_colors=NodeStyles(),
+                    )
+                )
+            )
+    except Exception as e:
+        print(f"[ERROR] Visualize Graph Error: {e}")
+
+
+def generate_random_hash():
+    return f"{random.randint(0, 0xffffff):06x}"
+
+## Jun: Mermaid를 사용하여 그래프 시각화
+def visualize_graph_mermaid(builder: StateGraph, output_file="mermaid/graph.html"):
+    print("Visualizing graph...")
+    nodes = []
+    edges = []
+
+    # builder.nodes: {노드_이름: 노드_정의_정보,...} 
+    # 단순히 노드 이름만 뽑아 그림에 표현
+    for node_name in builder.nodes:
+        nodes.append(f'{node_name}["{node_name}"]')
+
+    # builder.edges: [(소스노드, 타겟노드), (소스노드, 타겟노드), ...] 형태라고 가정
+    for (source_node, target_node) in builder.edges:
+        edges.append(f'{source_node} --> {target_node}')
+
+    mermaid_code = "graph TD\n" + "\n".join(nodes + edges)
+    html_content = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <script type="module">
+            import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+            mermaid.initialize({{ startOnLoad: true }});
+        </script>
+    </head>
+    <body>
+        <div class="mermaid">
+        {mermaid_code}
+        </div>
+    </body>
+    </html>
+    """
+
+    try:
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+        with open(output_file, "w") as file:
+            file.write(html_content)
+        print(f"Graph saved to {os.path.abspath(output_file)}")
+    except Exception as e:
+        print(f"Failed to save graph: {e}")
